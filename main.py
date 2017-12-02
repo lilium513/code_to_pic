e179bba5fb5242929a470566c19d7cbbb0a710d5
lilium <lim.intefx@gmail.com>
2017-12-03 02:05:56 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from PIL import Image
+from PIL import ImageDraw
+
+def drawing(img,imgae_array):
+    x,y = img.size
+    draw = ImageDraw.Draw(img)
+    for i in range(0,x):
+        for j in range(0,y):
+            if imgae_array[j][i]==1:
+                draw.point((i,j),(0x00))
+
+    return img
+
+def make_image(screen, bgcolor, filename,imgae_array):
+    img = Image.new('L', screen, bgcolor)
+    img = drawing(img,imgae_array)
+    img.save(filename)
+
+if __name__ == '__main__':
+    f = open('source.py')
+    lines = f.readlines()
+    f.close()
+    width = 0
+    for line in lines:
+        if len(line) > width:
+            width = len(line)
+
+    height = len(lines)
+
+    screen = (width,height) #横 縦
+
+    bgcolor=0xff
+    imgae_array = [[0 for i in range(width)] for j in range(height)]
+    for i,s in enumerate(lines):
+        for j,w in enumerate(s):
+            if(w!=' ' and w != '¥t'):
+                imgae_array[i][j] = 1
+    filename = "source.png"
+    make_image(screen, bgcolor, filename,imgae_array)
---
+++ source.py
@@ -0,0 +1,18 @@
+print ("hello world")
+print ("hello world")
+print ("hello world")
+print ("hello world")
+print ("hello world")
+for i in range(100):
+    for j in range(10):
+        for k in range(100):
+            for l in range(10):
+                for m in range(100):
+                    for n in range(10):
+                        print(i+j+k+l+m+n)
+    #6重ループを回すプログラム
+    #6重ループを回すプログラム
+    #6重ループを回すプログラム
+    #6重ループを回すプログラム
+    #6重ループを回すプログラム
+#6重ループを回すプログラム
